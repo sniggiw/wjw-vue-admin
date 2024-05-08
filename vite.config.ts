3d34575be4c1c5aac40649812c094ef9ab16d979
sniggiw <bewjweng@163.com>
2024-05-08 23:57:33 +0800

    feat:about config of vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import alias from './vite/alias';
+import { parseEnv } from './vite/util';
 
-export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
+// export default defineConfig({
+//     plugins: [vue()],
+//     resolve: {
+//         alias,
+//     },
+// });
+
+export default defineConfig(({ command, mode }) => {
+    // 在开发环境下 command 的值为 serve，而在生产环境下为 build
+    // mode 是指运行模式，也就是 .env[mode] 文件中的 mode
+    const isBulid = command === 'build';
+    const env = loadEnv(mode, process.cwd());
+
+    // parseEnv(env)
+
+    return {
+        plugins: [vue()],
+        resolve: {
+            alias,
         },
-    },
+    };
 });
---
+++ vite/util.ts
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+
+// 将 env 环境变量的值转换为原本定义的类型（env 文件中定义的变量默认全部会被转换成 string 类型）
+export function parseEnv(env: Record<string, any>) {
+    const envCopy = _.cloneDeep(env);
+    Object.entries(envCopy).forEach(([key, value]) => {
+        if (value === 'true' || value === 'false') {
+            envCopy[key] = value === 'true';
+        }
+        if (/^\d+$/.test(value)) {
+            envCopy[key] = Number(value);
+        }
+    });
+
+    return envCopy;
+}
